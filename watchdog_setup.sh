8bd66b51da2c473287b7ed43d9434a3a29cd7405
Mike White <47200069+dipstah@users.noreply.github.com>
2021-01-03 00:29:11 -0600

    Update watchdog_setup.sh

--- watchdog_setup.sh
+++ watchdog_setup.sh
@@ -8,14 +8,17 @@
 #Author         :Mike White
 #Email         	:dipstah@dippydawg.net
 ###################################################################
- 
+
+#install packages
 sudo apt-get install watchdog chkconfig
 
+#update config files
 sudo sed -i "s|#watchdog-device|watchdog-device|g" /etc/watchdog.conf
 sudo sed -i "s|#max-load-1|max-load-1|g" /etc/watchdog.conf
 
 echo bcm2835_wdt | sudo tee -a /etc/modules
 echo bcm2835_wdt | sudo tee /etc/modules-load.d/bcm2835_wdt.conf
 
+#start service
 chkconfig watchdog on
 sudo /etc/init.d/watchdog start
